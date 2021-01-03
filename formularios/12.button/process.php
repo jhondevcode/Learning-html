7ff67c4b663a0793653974c809d695fc309050ec
jhondev-code <jhondev.code@gmail.com>
2021-01-02 23:04:54 -0500

    Avance del 02-01-2021

---
+++ formularios/12.button/process.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Uso del elemento button">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <title>Uso de button</title>
+    </head>
+    <body>
+        <header>
+            <h1>Uso del componente button</h1>
+        </header>
+        <main>
+            <section>
+                <?php
+                    $cont = 0;
+                    while ($cont < 10) {
+                        echo $cont;
+                    }
+                ?>
+            </section>
+        </main>
+        <footer>
+            <small>Copyright &copy 2018 Copyright Holder All Rights Reserved.</small>
+        </footer>
+    </body>
+</html>
